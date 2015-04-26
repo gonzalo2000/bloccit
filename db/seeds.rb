d35ac1bc14faa4b0581446674eaa86e9a2109ff6
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2015-04-26 19:55:38 -0400

    Completed the assignment using where method and if statements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,29 @@
 require 'faker'
 
-#Create Posts
-50.times do 
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-  )
-end
+ #Create Posts
+ 50.times do 
+   Post.create!(
+     title: Faker::Lorem.sentence,
+     body:  Faker::Lorem.paragraph
+   )
+ end
 posts = Post.all
 
 #Create Comments
-100.times do 
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-  )
+ 100.times do 
+   Comment.create!(
+     post: posts.sample,
+     body: Faker::Lorem.paragraph
+   )
+ end
+
+if Post.where("title = 'This newer title'") == []
+  post_custom = Post.create!(title: "This newer title", body: "More unique still.")  
 end
 
- #Post.create!(title: "New Post Title", body: "More unique still.")
-
-# Comment.create!(
-#   post: post,
-#   body: ""
-# )
+if Comment.where("body = 'Like Nicki Minaj'") == []
+  Comment.create!(post: post_custom, body: "Like Nicki Minaj")
+end
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
